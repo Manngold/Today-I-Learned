50efb848f38e0b0c4da419e45196c060ab5dd824
Manngold <jsw4820@gmail.com>
2020-01-17 21:49:55 +0900

    200117 Node.js study record

--- Javascript/Node.js/main.js
+++ Javascript/Node.js/main.js
@@ -14,22 +14,9 @@ var app = http.createServer(function(request, response) {
         response.writeHead(404);
     }
     if (title === "HTML") {
-        fs.readFile("./data/HTML", "utf-8", function(err, data) {
+        fs.readFile(`./data/${title}`, "utf-8", function(err, data) {
             content = data;
-        });
-    } else if (title === "CSS") {
-        fs.readFile("./data/CSS", "utf-8", function(err, data) {
-            content = data;
-        });
-    } else if (title === "JavaScript") {
-        fs.readFile("./data/JavaScript", "utf-8", function(err, data) {
-            content = data;
-        });
-    } else {
-        content = "Welcome";
-    }
-    response.writeHead(200);
-    var template = `<!doctype html>
+            var template = `<!doctype html>
     <html>
     <head>
       <title>WEB1 - ${title}</title>
@@ -49,6 +36,82 @@ var app = http.createServer(function(request, response) {
     </body>
     </html>
     `;
-    response.end(template);
+            response.end(template);
+        });
+    } else if (title === "CSS") {
+        fs.readFile(`./data/${title}`, "utf-8", function(err, data) {
+            content = data;
+            var template = `<!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="index.html">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+      <img src="coding.jpg" width="100%">
+      </p><p style="margin-top:45px;">${content}</p>
+    </body>
+    </html>
+    `;
+            response.end(template);
+        });
+    } else if (title === "JavaScript") {
+        fs.readFile(`./data/${title}`, "utf-8", function(err, data) {
+            content = data;
+            var template = `<!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="index.html">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+      <img src="coding.jpg" width="100%">
+      </p><p style="margin-top:45px;">${content}</p>
+    </body>
+    </html>
+    `;
+            response.end(template);
+        });
+    } else {
+        content = "Welcome";
+        var template = `<!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="index.html">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+      <img src="coding.jpg" width="100%">
+      </p><p style="margin-top:45px;">${content}</p>
+    </body>
+    </html>
+    `;
+        response.end(template);
+    }
+    response.writeHead(200);
 });
 app.listen(3000);
